bab3532ef006e0428cd08dab6af8caf6e8b5350f
Rakhi2421 <srirakesh242001@gmail.com>
2025-09-04 04:31:00 +0530

    changes done in avail zone

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable subnet_cidr_block {
     default = "10.0.10.0/24"
 }
 variable avail_zone {
-    default = "eu-west-3a"
+    default = "us-east-1"
 }
 variable env_prefix {
     default = "dev"
@@ -20,5 +20,5 @@ variable instance_type {
     default = "t2.micro"
 }
 variable region {
-    default = "eu-west-3"
+    default = "us-east-1"
 }
